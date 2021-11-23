f2d2e77253c40e151419ecb17fa4af273912484b
freesudani <mohamed.dahiya@hotmail.com>
2021-11-23 15:22:56 +0700

    Home & Adjusting CSS

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Typography } from "@mui/material";
 
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Typography>Hello word</Typography>
+    <div className={styles.main}>
+      <h1 className={styles.header}>D-News News App</h1>
+      <h3>Your Most trusted source for daily news</h3>
     </div>
   );
 }
