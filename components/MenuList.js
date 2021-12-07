78f8664e08d376bc2e52bfad7f350fd2baeefd0a
freesudani <mohamed.dahiya@hotmail.com>
2021-12-07 14:35:57 +0700

    fixing css adding masonry layout

--- components/MenuList.js
+++ components/MenuList.js
@@ -1,19 +1,27 @@
 import React from "react";
 import SingleMeal from "./SingleMeal";
 import classes from "../styles/MenuList.module.css";
+import Masonry from "react-masonry-css";
 
 const MenuList = (props) => {
+  const breakpoints = { default: 3, 1100: 2, 700: 1 };
   return (
-    <ul className={classes.list}>
-      {props.meals.map((meal) => (
-        <SingleMeal
-          key={meal.id}
-          id={meal.id}
-          image={meal.image}
-          title={meal.title}
-        />
-      ))}
-    </ul>
+    <div className={classes.list}>
+      <Masonry
+        breakpointCols={breakpoints}
+        className={classes.mymasonrygrid}
+        columnClassName={classes.mymasonrygridcolumn}
+      >
+        {props.meals.map((meal) => (
+          <SingleMeal
+            key={meal.id}
+            id={meal.id}
+            image={meal.image}
+            title={meal.title}
+          />
+        ))}
+      </Masonry>
+    </div>
   );
 };
 
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,29 +1,38 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import styles from "../styles/Navbar.module.css";
+import classes from "../styles/Navbar.module.css";
+import { useRouter } from "next/router";
 
 const menuItems = [
-  { text: "About", path: "/" },
-  { text: "Add New Meal", path: "/new-meal" },
+  { text: "Meals List", path: "/" },
+  { text: "Add Meal", path: "/new-meal" },
 ];
 
 const Navbar = () => {
+  const router = useRouter();
+
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.logo}>
+    <nav className={classes.navbar}>
+      <div className={classes.logo}>
         <Image
-          src="/d2672d31768e410b8d39134d631014d2.png"
+          src="/6287c1323c62428191b8fcaf3c633b96.png"
           alt="site logo"
-          width={170}
-          height={100}
+          width={240}
+          height={160}
         />
       </div>
-      <div className={styles.links}>
+      <div className={classes.links}>
         {menuItems.map((item, index) => {
           return (
             <Link href={item.path} key={index}>
-              <a className={styles.anchor}>{item.text}</a>
+              <a
+                className={
+                  router.pathname == item.path ? classes.active : classes.anchor
+                }
+              >
+                {item.text}
+              </a>
             </Link>
           );
         })}
